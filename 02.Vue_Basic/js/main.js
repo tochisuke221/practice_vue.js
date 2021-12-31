d9257c6b5791f967e4bcc67bfd519ab3f83843af
tochisuke221 <tochisuke221@gmail.com>
2021-12-31 17:59:00 +0900

    Vue.js 基本

---
+++ 02.Vue_Basic/js/main.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: '#app',
+  data: {
+    styleObject:{
+      color: 'red',
+      'background-color': 'blue'
+    },
+    baseStyle: {
+      'font-weight': '20px'
+    }
+  },
+})
