7d7a8c8e5124a80137d8a742fb910eb99e4f43ec
tochisuke221 <tochisuke221@gmail.com>
2022-01-09 22:53:27 +0900

    v-modelで色々なフォームを作る

---
+++ 09.Vue_8/udemy-vuejs/src/components/LikeNumber.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <p>
+      いいね({{ halfNumber }})
+    </p>
+    <button @click="increment()">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    totalNumber: {
+      type: Number,
+      // required: true 
+      default: 10,
+    },
+    testProps: {
+      type: Number,
+      // required: true 
+      default: 10,
+    },
+  },
+  computed: {
+    halfNumber(){
+      return this.totalNumber / 2
+    }
+  },
+  methods: {
+    increment() {
+      this.$emit('my-click', this.totalNumber + 1 );
+      // this.totalNumber += 1;
+    },
+  },
+};
+</script>
