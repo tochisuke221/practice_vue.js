571d0b5a2f8d1c90d7a0e8abcce1450b7da8dcb5
tochisuke221 <tochisuke221@gmail.com>
2022-01-09 14:09:09 +0900

    高度なコンポーネント処理

---
+++ 08.Vue_7/udemy-vuejs/src/components/LikeHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+
+export default{
+  props: ["headerText"],
+  data(){
+    return{
+      user: {
+        firstName: "栃川",
+        lastName: "晃佑",
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  h1{
+    color: red;
+  }
+</style>>
